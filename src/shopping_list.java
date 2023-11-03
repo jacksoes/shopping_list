7768cb5a267d7b6e4346809701b4b3d4ab66453a
jacksoes <jsoesman1@yahoo.com>
2023-11-02 21:38:12 -0400

    create shopping_list object with a get.self method

--- src/shopping_list.java
+++ src/shopping_list.java
@@ -1,6 +1,64 @@
-public class shopping_list {
+import java.util.Scanner;
+
+public class shopping_list
+{
+    // constructs node
+    public static class node
+    {
+        int data; node next;
+        public node(int data) {this.data = data; this.next = null;}
+    }
+    // type in add command
+    private node head;
+    public shopping_list(node head)
+    {
+        this.head = head;
+    }
+    public node get_head()
+    {
+        return this.head;
+    }
+
+    public void print_list(node head)
+    {
+        while (head != null)
+        {
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+
+    public int get_data(node head)
+    {
+        return head.data;
+    }
+
     public static void main(String[] args) throws Exception 
     {
+
+        //accept input until given name 
+
+        Scanner kbd = new Scanner(System.in);
+        node jack = new node(26);
+        node jack1 = new node(23);
+        node jack2 = new node(25);
+
+
+        jack.next = jack1;
+        jack1.next = jack2;
+
+        shopping_list test = new shopping_list(jack);
+
+        test.print_list(test.get_head());
+
+       // System.out.println(test.get_data( test.get_head() ) );
+        //test.print_list( test.get_head() );
+        
+
+
+
+
+        /* 
         //check for valid command
         try {args[0].length();}
         catch ( IndexOutOfBoundsException e ) 
@@ -23,7 +81,7 @@ public class shopping_list {
                 command_add();
                 break;
             case "remove":
-                /*command_remove();*/
+                // command_ remove
                 System.out.println("Remove coming soon!");
                 break;
             default:
@@ -34,11 +92,17 @@ public class shopping_list {
         }
         System.out.println("testing git");
         return;
+        */
         //System.out.println("this is zero: " + args[0] + "\nthis is one: " + args[1]);
     }
 
 
 
+    /*
+     * make new function to create a linked list 
+     *  
+     * 
+     */
     public static void command_add()
     {
        
